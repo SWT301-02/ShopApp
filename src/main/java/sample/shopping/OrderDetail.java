7de246b33a5f8787e307966ee57fb51311dec350
DuongNManh <manhduonglhp4@gmail.com>
2024-09-24 22:39:30 +0700

    test ci

--- src/main/java/sample/shopping/OrderDetail.java
+++ src/main/java/sample/shopping/OrderDetail.java
@@ -1,1 +1,25 @@
- 
+package sample.shopping;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tblOrderDetail")
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String productID;
+    private double price;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "orderID", insertable = false, updatable = false)
+    private Order order; // This establishes the relationship with Order
+}
